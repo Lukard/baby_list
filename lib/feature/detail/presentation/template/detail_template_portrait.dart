353e3f2bbc385df707ca3023ebd34c19d9b72209
Rubén Abad <rubenabad89@gmail.com>
2021-05-09 07:57:57 +0200

    Remove unused imports

--- lib/feature/detail/presentation/template/detail_template_portrait.dart
+++ lib/feature/detail/presentation/template/detail_template_portrait.dart
@@ -1,7 +1,5 @@
 import 'package:baby_list/core/domain/model/baby_list.dart';
 import 'package:baby_list/feature/detail/presentation/organism/item_column.dart';
-import 'package:baby_list/feature/detail/presentation/organism/participation.dart';
-import 'package:baby_list/xds/atom/stadium_button.dart';
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 
